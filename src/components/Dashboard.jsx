c31173e640a741df0896919ab622ad93a5dcdf4e
Raghvendra <raghvendramadhi@gmail.com>
2026-05-10 21:14:14 +0530

    Add Dashboard and Login page

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,13 @@
+
+
+const Dashbaord = () => {
+
+    <div className="h-screen flex items-center justify-center bg-black">
+      <h1 className="text-5xl font-bold text-green-400 ">
+            Dashboard
+        </h1>
+    </div>;
+    
+}
+
+export default Dashbaord;
---
+++ src/components/Login.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+const Login = () => {
+  <div className="h-screen flex items-center justify-center bg-black">
+      <h1 className="text-5xl font-bold text-green-400 ">
+        Login
+    </h1>
+  </div>
+};
+
+export default Login;
