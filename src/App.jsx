47b1629a99451212cd368e1d06690416c0ef072e
Raghvendra <raghvendramadhi@gmail.com>
2026-05-10 13:47:53 +0530

    Added README documentation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 function App() {
   return (
-    <div>
-      <h1>Hello React 🚀</h1>
+    <div className="h-screen flex items-center justify-center bg-black">
+      <h1 className="text-5xl font-bold text-green-400 ">
+        React Project 🚀
+      </h1>
     </div>
   );
 }
